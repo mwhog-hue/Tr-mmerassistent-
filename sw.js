f845960c974830a11b64ea74a237830ab92c4e1c
mwhog-hue <mweisse.hog@gmail.com>
2026-09-23 13:25:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* RH-Trümmersuchassistent – Service Worker: startet die App auch ohne Internet (z. B. im Funkloch).
    Die Daten liegen im lokalen Gerätespeicher, nicht in diesem Cache.
    Bei jeder neuen Version CACHE_VERSION erhöhen. */
-const CACHE_VERSION = 'truemmersuche-2.1.2';
+const CACHE_VERSION = 'truemmersuche-2.1.3';
 const DATEIEN = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_VERSION).then(c=>c.addAll(DATEIEN)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_VERSION).map(x=>caches.delete(x)))).then(()=>self.clients.claim())); });
